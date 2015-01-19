05f5b0ab7ef0c8cabc1219c790625beae6508bdd
evgeniy-baba <evgenijbaba@gmail.com>
2015-01-19 23:51:29 +0600

    Add files

---
+++ README.md
@@ -0,0 +1,7 @@
+dotfiles
+========
+
+## Install
+
+git clone this_repo .dotfiles  
+cd .dotfiles && ./bootstrap.sh
---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# script to symlink files in ~/.dotfiles to the home directory
+
+files=~/.dotfiles/.*
+ignorefiles=(".gitignore" ".gitmodules")
+
+for full_path in $files
+do
+    file=`echo $full_path | cut -d'/' -f 5`
+
+    if [[ ${ignorefiles[*]} =~ "$file" ]]
+    then
+	echo "$file passed (ignored file list)"
+    else
+        if [ ! -h ~/$file ]
+        then
+            echo "Creating symlink for $file"
+            ln -s $full_path ~/$file
+        else
+            echo "Config already exists for $file"
+        fi
+    fi
+done
+ 
+echo "Finished creating symlinks."
